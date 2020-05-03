29e8d5348b342ac34be1bb1b868e0d4295ce3ae9
diego davel <diegodavel@gmail.com>
2020-05-03 15:59:43 -0300

    .

--- frontend/otmmto-web/src/app/app.module.ts
+++ frontend/otmmto-web/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LayoutBaseComponent } from './components/layout-base/layout-base.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LayoutBaseComponent
   ],
   imports: [
     BrowserModule,
--- frontend/otmmto-web/src/app/pages/home/home.component.ts
+++ frontend/otmmto-web/src/app/pages/home/home.component.ts
@@ -7,16 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  contagem: number;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.contagem = 0;
-  }
-
-  add(){
-    this.contagem++;
   }
 
 }
