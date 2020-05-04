c60f843ecbe36664356e5e252b4d479f8062192b
diego davel <diegodavel@gmail.com>
2020-05-04 20:19:07 -0300

    .

--- frontend/otmmto-web/src/app/app.module.ts
+++ frontend/otmmto-web/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { LayoutBaseComponent } from './components/layout-base/layout-base.component';
+import { CadastrarUsuarioComponent } from './pages/cadastrar-usuario/cadastrar-usuario.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LayoutBaseComponent
+    LayoutBaseComponent,
+    CadastrarUsuarioComponent
   ],
   imports: [
     BrowserModule,
